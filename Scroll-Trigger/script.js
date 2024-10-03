07540f2a1a37072f6effbe15bc88460598bb8074
Zodvinex <159710005+Zodvinex@users.noreply.github.com>
2024-10-04 00:00:04 +0530

    Add files via upload

---
+++ Scroll-Trigger/script.js
@@ -0,0 +1,71 @@
+gsap.to('.card1',{
+    scale: 0.7,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: ".card1",
+        start: "top 15%",
+        end: "bottom 15%",
+        markers: true,
+        scrub: true,
+}
+})
+gsap.to('.card2',{
+    scale: 0.7,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: ".card2",
+        start: "top 15%",
+        end: "bottom 15%",
+        markers: true,
+        scrub: true,
+
+}
+})
+gsap.to('.card3',{
+    scale: 0.7,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: ".card3",
+        start: "top 15%",
+        end: "bottom 15%",
+        markers: true,
+        scrub: true,
+
+}
+})
+gsap.to('.card4',{
+    scale: 0.7,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: ".card4",
+        start: "top 15%",
+        end: "bottom 15%",
+        markers: true,
+        scrub: true,
+
+}
+})
+gsap.to('.card5',{
+    scale: 0.7,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: ".card6",
+        start: "top 15%",
+        end: "bottom 15%",
+        markers: true,
+        scrub: true,
+
+}
+})
+gsap.to('.card6',{
+    scale: 0.7,
+    opacity: 0,
+    scrollTrigger:{
+        trigger: ".card6",
+        start: "top 15%",
+        end: "bottom 15%",
+        markers: true,
+        scrub: true,
+
+}
+})
